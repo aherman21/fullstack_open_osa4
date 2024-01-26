b9498f7de5c5cc7da5c2b3f330d7f7d79ddda951
aherman21 <hermansalexander2@gmail.com>
2024-01-26 14:50:08 +0200

    user model and controller made, tests need to be done next

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const mongoose = require('mongoose')
 
 mongoose.set('strictQuery', false)
@@ -21,6 +22,7 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 
 
