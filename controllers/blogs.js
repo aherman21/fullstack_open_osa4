8ef2b7bcffab15ed6da69b5004d9cb73eda7ace7
aherman21 <hermansalexander2@gmail.com>
2024-02-05 18:57:47 +0200

    token confirmation when posting started

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,7 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
 	const blogs = await Blog.find({})
@@ -9,10 +10,22 @@ blogsRouter.get('/', async (request, response) => {
 }
 )
 
+const getTokenFrom = request => {
+	const authorization = request.get('authorization')
+	if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+		return authorization.replace('bearer ', '')
+	}
+	return null
+}
+
 blogsRouter.post('/', async (request, response) => {
 	const body = request.body
+	const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+	if (!request.token || !decodedToken.id) {
+		return response.status(401).json({ error: 'token missing or invalid' })
+	}
+	const user = await User.findById(decodedToken.id)
 
-	const user = await User.findOne()
 
 	const populatedUser = await User.findById(user._id).populate('blogs', { url: 1, title: 1, author: 1 })
 	console.log(populatedUser)
