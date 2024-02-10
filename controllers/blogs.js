40ab10b2f5f5e53ad170f4f3f226bf7c3c528a75
aherman21 <hermansalexander2@gmail.com>
2024-02-10 15:11:20 +0200

    4.19

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -12,8 +12,8 @@ blogsRouter.get('/', async (request, response) => {
 
 const getTokenFrom = request => {
 	const authorization = request.get('authorization')
-	if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-		return authorization.replace('bearer ', '')
+	if (authorization && authorization.startsWith('Bearer ')) {
+		return authorization.replace('Bearer ', '')
 	}
 	return null
 }
@@ -21,8 +21,8 @@ const getTokenFrom = request => {
 blogsRouter.post('/', async (request, response) => {
 	const body = request.body
 	const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
-	if (!request.token || !decodedToken.id) {
-		return response.status(401).json({ error: 'token missing or invalid' })
+	if (!decodedToken.id) {
+		return response.status(401).json({ error: 'token invalid' })
 	}
 	const user = await User.findById(decodedToken.id)
 
--- utils/middleware.js
+++ utils/middleware.js
@@ -1,3 +1,17 @@
+const logger = require('./logger')
+
+const requestLogger = (request, response, next) => {
+	logger.info('Method:', request.method)
+	logger.info('Path:  ', request.path)
+	logger.info('Body:  ', request.body)
+	logger.info('---')
+	next()
+}
+
+const unknownEndpoint = (request, response) => {
+	response.status(404).send({ error: 'unknown endpoint' })
+}
+
 const errorHandler = (error, request, response, next) => {
 	console.error(error.message)
 	if (error.name === 'CastError') {
@@ -10,4 +24,4 @@ const errorHandler = (error, request, response, next) => {
 	next(error)
 }
 
-module.exports = errorHandler
+module.exports = {errorHandler, unknownEndpoint, requestLogger}
